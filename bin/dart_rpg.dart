9bdb331c78b063ed376b952af0e39376febe634f
Daeseong Kim <daeseongkim05@gmail.com>
2024-11-06 05:26:51 +0900

    feat: add startGame and getRandomMonster

--- bin/dart_rpg.dart
+++ bin/dart_rpg.dart
@@ -1,3 +1,8 @@
-void main(List<String> arguments) {
-  print("init setting");
+import 'package:dart_rpg/class/game.dart';
+
+void main(List<String> arguments) async {
+  Game game = Game();
+
+  await game.startGame();
+  await game.getRandomMonster();
 }
