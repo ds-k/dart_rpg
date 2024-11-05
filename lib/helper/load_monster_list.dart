d6e9fac9b458e3e5d68897c91136254c4dd130b6
Daeseong Kim <daeseongkim05@gmail.com>
2024-11-06 05:26:00 +0900

    fix: change logic of load mosters data

--- lib/helper/load_monster_list.dart
+++ lib/helper/load_monster_list.dart
@@ -6,13 +6,15 @@ Future<List<Monster>?> loadMonsterListAsync() async {
     final file = File('assets/db/monsters.csv');
     final lines = await file.readAsLines();
     List<Monster> monsters = [];
-    for (var line in lines) {
-      monsters.add(Monster(line[0], int.parse(line[1]), int.parse(line[2])));
+    for (String line in lines) {
+      List<dynamic> lineList = line.split(",");
+      monsters.add(
+          Monster(lineList[0], int.parse(lineList[1]), int.parse(lineList[2])));
     }
     if (monsters.length != 3) throw FormatException('Invalid monsters data');
     return monsters;
   } catch (e) {
-    print('캐릭터 데이터를 불러오는 데 실패했습니다: $e');
+    print('몬스터 데이터를 불러오는 데 실패했습니다: $e');
     return null;
   }
 }
