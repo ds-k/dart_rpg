3a112b8205ada920812e57eec0e56bd2ff9a005b
Daeseong Kim <daeseongkim05@gmail.com>
2024-11-06 04:42:38 +0900

    add: add helper function for load monster list

---
+++ lib/helper/load_monster_list.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+import 'package:dart_rpg/class/character.dart';
+import 'package:dart_rpg/class/monster.dart';
+
+Future<List<Monster>?> loadMonsterListAsync() async {
+  try {
+    final file = File('assets/db/monsters.csv');
+    final lines = await file.readAsLines();
+    List<Monster> monsters = [];
+    for (var line in lines) {
+      monsters.add(Monster(line[0], int.parse(line[1]), int.parse(line[2])));
+    }
+    if (monsters.length != 3) throw FormatException('Invalid monsters data');
+    return monsters;
+  } catch (e) {
+    print('캐릭터 데이터를 불러오는 데 실패했습니다: $e');
+    return null;
+  }
+}
