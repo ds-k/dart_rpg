ae0952130990b3adeec584cf2323377cdf8d6b7a
Daeseong Kim <daeseongkim05@gmail.com>
2024-11-07 23:09:11 +0900

    add : add comment(monster class)

--- lib/class/game.dart
+++ lib/class/game.dart
@@ -7,6 +7,7 @@ import 'dart:io';
 import 'package:dart_rpg/helper/load_character_stats.dart';
 import 'package:dart_rpg/helper/load_monster_list.dart';
 import 'package:dart_rpg/util/check_regex.dart';
+import 'package:dart_rpg/util/check_save.dart';
 
 class Game {
   Character? character;
@@ -33,56 +34,70 @@ class Game {
 
   void battle() async {
     Monster randomMonster = await getRandomMonster();
-    // 캐릭터의 체력이 0 이하가 되면 게임이 종료
+
     print("\n새로운 몬스터가 나타났습니다!");
     print(
         "${randomMonster.name} - 체력: ${randomMonster.hp}, 공격력: ${randomMonster.attack}");
+
+    // 캐릭터의 체력이 0 이하가 되면 게임이 종료
     while (character!.hp > 0) {
-      // 캐릭터의 행동
+      // 캐릭터의 턴
       print("\n${character!.name}의 턴");
       print("행동을 선택하세요 (1: 공격, 2: 방어)");
 
-      switch (stdin.readLineSync() as String) {
-        case "1":
-          character!.attackMonster(randomMonster);
-          // 몬스터 체력이 0 이하
-          if (randomMonster.hp <= 0) {
-            print("${randomMonster.name}을 물리쳤습니다!");
-            monsterKillCount += 1;
-            print("\n다음 몬스터와 싸우시겠습니까?");
-            switch (stdin.readLineSync() as String) {
-              case "y":
-              case "Y":
-                monsterList!.remove(randomMonster);
-                print(monsterList);
-                battle();
+      try {
+        switch (stdin.readLineSync() as String) {
+          case "1":
+            character!.attackMonster(randomMonster);
+            // 몬스터가 죽은 경우
+            if (randomMonster.hp <= 0) {
+              print("${randomMonster.name}을 물리쳤습니다!");
+              monsterList!.remove(randomMonster);
+              monsterKillCount += 1;
+              if (monsterList!.isEmpty) {
+                print("축하합니다! 모든 몬스터를 물리쳤습니다.");
+                checkSave(character!, true);
                 return;
-              case "n":
-              case "N":
-                print("게임을 종료합니다. 도중에 종료한 게임은 저장되지 않습니다.");
-                return;
+              }
+
+              print("\n다음 몬스터와 싸우시겠습니까? (y/n)");
+              switch (stdin.readLineSync() as String) {
+                case "y":
+                case "Y":
+                  battle();
+                  return;
+                case "n":
+                case "N":
+                  print("게임을 종료합니다. 도중에 종료한 게임은 저장되지 않습니다.");
+                  return;
+                default:
+                  throw Exception("잘못된 선택입니다. 게임을 다시 시작해 주세요.");
+              }
             }
+          case "2":
+            character!.defend(randomMonster);
             break;
-          }
-        case "2":
-          character!.defend(randomMonster);
-          break;
+          default:
+            throw Exception("잘못된 선택입니다. 게임을 다시 시작해 주세요.");
+        }
+      } catch (e) {
+        print(e.toString().substring(11));
+        return;
       }
-      // 몬스터의 행동
+      // 몬스터의 턴
       print("\n${randomMonster.name}의 턴");
       randomMonster.attackCharacter(character!);
 
-      if (character!.hp <= 0) {
-        return;
+      // 캐릭터가 죽지 않았을 경우 진행을 위해 캐릭터와 몬스터의 상태 노출
+      if (character!.hp > 0) {
+        character!.showStatus();
+        randomMonster.showStatus();
       }
-
-      character!.showStatus();
-      randomMonster.showStatus();
     }
 
-    // 캐릭터 죽음
-    print("\n${character!.name}이 죽었습니다. 다음에 다시 도전해 주세요.");
-    return;
+    // 캐릭터가 죽은 경우
+    print("\n${character!.name}이(가) 죽었습니다. 다음에 다시 도전해 주세요.");
+    checkSave(character!, false);
   }
 
   Future<Monster> getRandomMonster() async {
@@ -91,7 +106,7 @@ class Game {
 
     Monster randomMonster = monsterList![randomIdx];
 
-    int min = character?.defense ?? 1;
+    int min = character!.defense;
     int max = randomMonster.attack;
 
     randomMonster.attack = random.nextInt(max - min) + min;
